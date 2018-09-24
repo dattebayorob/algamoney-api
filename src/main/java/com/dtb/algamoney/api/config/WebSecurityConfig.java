c4dce0d8e9c4e9b72cfcf02dd864f2467ec7d8d2
Robson William <robson.william65@gmail.com>
2018-09-24 00:02:20 -0300

    Autenticacao oauth, em memoria por enquanto

---
+++ src/main/java/com/dtb/algamoney/api/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.dtb.algamoney.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableWebSecurity
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) 
+      throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("admin").password("admin").roles("ROLE");
+    }
+	@Bean
+	@SuppressWarnings("deprecation")
+	 public static NoOpPasswordEncoder passwordEncoder() {
+	     return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	 }
+	//public PasswordEncoder passwordEncoder() {
+	//	return new BCryptPasswordEncoder();
+	//}
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+	
+	
+	
+}
