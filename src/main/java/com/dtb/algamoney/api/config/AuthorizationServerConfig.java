e1fab712b95956e5e0749f315766c43640a2660c
Robson William <robson.william65@gmail.com>
2018-09-27 08:59:16 -0300

    tokens jwt, refresh_token em cookie. Obs. Refresh_token precisa ser fixado

--- src/main/java/com/dtb/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/dtb/algamoney/api/config/AuthorizationServerConfig.java
@@ -4,15 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -32,19 +31,27 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 		clients.inMemory().withClient("angular")
 		.secret("angular")
 		.scopes("read","write")
-		.authorizedGrantTypes("password")
+		.authorizedGrantTypes("password","refresh_token")
 		.scopes("all")
-		.accessTokenValiditySeconds(1800);
+		.accessTokenValiditySeconds(20)
+		.refreshTokenValiditySeconds(3600*24);
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
+		.reuseRefreshTokens(false)
 		.authenticationManager(authenticationManager);
 	}
-	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("qwe123");
+		return accessTokenConverter;
+	}
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
